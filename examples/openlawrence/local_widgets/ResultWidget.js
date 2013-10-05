ec0e36526eb8eea862c77850926cd13e83c1beaa
James Michael DuPont <jamesmikedupont@gmail.com>
2013-10-05 14:46:51 -0700

    better quote

---
+++ examples/openlawrence/local_widgets/ResultWidget.js
@@ -0,0 +1,67 @@
+(function (callback) {
+  if (typeof define === 'function' && define.amd) {
+    define(['core/AbstractWidget'], callback);
+  }
+  else {
+    callback();
+  }
+}(function () {
+
+(function ($) {
+
+AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
+  start: 0,
+
+  beforeRequest: function () {
+    $(this.target).html($('<img>').attr('src', 'images/ajax-loader.gif'));
+  },
+
+  afterRequest: function () {
+    $(this.target).empty();
+    for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
+      var doc = this.manager.response.response.docs[i];
+      $(this.target).append(this.template(doc));
+
+      var items = [];
+
+      var $links = $('#links_' + doc.id);
+      $links.empty();
+      for (var j = 0, m = items.length; j < m; j++) {
+        $links.append($('<li></li>').append(items[j]));
+      }
+    }
+  },
+
+  template: function (doc) {
+    var snippet = '';
+    if (!doc) {
+        return "error1";
+    }
+      
+    return "<a href='" + doc.url + "'>" + doc.title + "</a><p>";
+
+
+  },
+
+  init: function () {
+    $(document).on('click', 'a.more', function () {
+      var $this = $(this),
+          span = $this.parent().find('span');
+
+      if (span.is(':visible')) {
+        span.hide();
+        $this.text('more');
+      }
+      else {
+        span.show();
+        $this.text('less');
+      }
+
+      return false;
+    });
+  }
+});
+
+})(jQuery);
+
+}));
